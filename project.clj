b5ebbe0e6ef5312e3d492dbde1d83804d280a55b
David Wilson <david@daviwil.com>
2014-10-30 22:11:34 -0700

    First commit of slightly modified Quil template

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject quil-overtone-lab "0.1.0-SNAPSHOT"
+  :description "A test project to integrate Quil and Overtone for procedural art and music."
+  :url "https://github.com/daviwil/quil-overtone-lab"
+  :license {:name "MIT"
+            :url "http://github.com/daviwil/quil-overtone-lab/blob/master/LICENSE" }
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.2"]])
